e7d54d3247a2999f738a8056fe3b7bf4f152b43e
Anjali2468 <83118136+Anjali2468@users.noreply.github.com>
2021-09-12 23:21:17 +0530

    Add files via upload

---
+++ DoInsertion.m
@@ -0,0 +1,13 @@
+function q=DoInsertion(p,i1,i2)
+    q=p;
+    if i1<i2
+        d=p(i1:i2-1);
+        d=[p(i2) d];
+        q(i1:i2)=d;
+    end
+    if i1>i2
+        d=p(i2+1:i1);
+        d=[d p(i2)];
+        q(i2:i1)=d;
+    end
+end
